2a1decb84ab0af2e00fea2edb041e283cc956988
Muhammad Daniyal <muhammad.8425450@talmeez.pk>
2017-12-16 20:11:55 +0500

    my first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import CustomeRoutes from 'Routes'
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<CustomeRoutes />, document.getElementById('root'));
+registerServiceWorker();
